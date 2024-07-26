edf45df323c02f4cb7d3eedce8868c8848002747
Britt Shook <brittanysshook@gmail.com>
2024-07-25 22:43:09 -0400

    create category table schema

--- backend/src/main/java/com/cragsupplyco/backend/models/Category.java
+++ backend/src/main/java/com/cragsupplyco/backend/models/Category.java
@@ -1,5 +1,39 @@
 package com.cragsupplyco.backend.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 50)
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + "]";
+    }
+
 }
