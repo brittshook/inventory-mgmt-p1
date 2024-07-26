b855211de45eec85bdca5e70275f9463ec2ca348
Britt Shook <brittanysshook@gmail.com>
2024-07-26 00:12:21 -0400

    swap to identity ref, add inventory link, update toString

--- backend/src/main/java/com/cragsupplyco/backend/models/Warehouse.java
+++ backend/src/main/java/com/cragsupplyco/backend/models/Warehouse.java
@@ -1,14 +1,23 @@
 package com.cragsupplyco.backend.models;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 @Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Warehouse {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +26,10 @@ public class Warehouse {
     @Column(length = 50)
     @NotBlank
     private String name;
+
+    @Min(value = 1)
+    @NotNull
+    private int maxCapacity;
 
     @Column(length = 255)
     @NotBlank
@@ -35,6 +48,9 @@ public class Warehouse {
     @NotBlank
     @Pattern(regexp = "^\\d{5}(-\\d{4})?$")
     private String zipCode;
+
+    @OneToMany(mappedBy = "warehouse")
+    private List<Inventory> inventory;
 
     public int getId() {
         return id;
@@ -84,10 +100,27 @@ public class Warehouse {
         this.zipCode = zipCode;
     }
 
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public List<Inventory> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<Inventory> inventory) {
+        this.inventory = inventory;
+    }
+
     @Override
     public String toString() {
-        return "Warehouse [id=" + id + ", name=" + name + ", streetAddress=" + streetAddress + ", city=" + city
-                + ", state=" + state + ", zipCode=" + zipCode + "]";
+        return "Warehouse [id=" + id + ", name=" + name + ", maxCapacity=" + maxCapacity + ", streetAddress="
+                + streetAddress + ", city=" + city + ", state=" + state + ", zipCode=" + zipCode + ", inventory="
+                + inventory + "]";
     }
 
 }
