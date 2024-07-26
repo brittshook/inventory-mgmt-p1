c9315e4b5df77564b0598a711e11c41afc03e6da
Britt Shook <brittanysshook@gmail.com>
2024-07-25 23:22:12 -0400

    create product table schema

--- backend/src/main/java/com/cragsupplyco/backend/models/Product.java
+++ backend/src/main/java/com/cragsupplyco/backend/models/Product.java
@@ -1,5 +1,84 @@
 package com.cragsupplyco.backend.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Product {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // strategies: IDENTITY or AUTO
+    private int id;
+
+    @Column(length = 255)
+    @NotBlank
+    private String name;
+
+    @Column(length = 1000)
+    @NotBlank
+    private String description;
+
+    @Column(scale = 2)
+    @NotNull
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @JsonBackReference
+    private Category category;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
+                + ", category=" + category + "]";
+    }
+
 }
