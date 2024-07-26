019001a2ca8ab1ca7d12d409bcfba4734ef2bd94
Britt Shook <brittanysshook@gmail.com>
2024-07-26 00:12:06 -0400

    swap to identity ref, add inventory link, update toString

--- backend/src/main/java/com/cragsupplyco/backend/models/Product.java
+++ backend/src/main/java/com/cragsupplyco/backend/models/Product.java
@@ -1,6 +1,10 @@
 package com.cragsupplyco.backend.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,10 +13,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // strategies: IDENTITY or AUTO
@@ -33,8 +39,11 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     @NotNull
-    @JsonBackReference
+    @JsonIdentityReference(alwaysAsId = true)
     private Category category;
+
+    @OneToMany(mappedBy = "product")
+    private List<Inventory> inventory;
 
     public int getId() {
         return id;
@@ -76,10 +85,18 @@ public class Product {
         this.category = category;
     }
 
+    public List<Inventory> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<Inventory> inventory) {
+        this.inventory = inventory;
+    }
+
     @Override
     public String toString() {
         return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
-                + ", category=" + category + "]";
+                + ", category=" + category + ", inventory=" + inventory + "]";
     }
 
 }
