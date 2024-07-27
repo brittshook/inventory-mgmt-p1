7c76e3a94eb33c5f7415dc8b2919584072982f94
Britt Shook <brittanysshook@gmail.com>
2024-07-27 19:27:23 -0400

    add basic services for product

---
+++ backend/src/main/java/com/cragsupplyco/backend/services/ProductService.java
@@ -0,0 +1,36 @@
+package com.cragsupplyco.backend.services;
+
+import java.util.Optional;
+
+import com.cragsupplyco.backend.models.Product;
+import com.cragsupplyco.backend.repositories.ProductRepository;
+
+public class ProductService {
+    private ProductRepository repo;
+
+    public ProductService(ProductRepository repo) {
+        this.repo = repo;
+    }
+
+    public Iterable<Product> findAll() {
+        return repo.findAll();
+    }
+
+    public Optional<Product> findById(int id) {
+        return repo.findById(id);
+    }
+
+    public Product save(Product product) {
+        return repo.save(product);
+    }
+
+    public Product updateProductById(int id, Product product) {
+        product.setId(id);
+        return repo.save(product);
+    }
+
+    public void deleteById(int id) {
+        repo.deleteById(id);
+    }
+
+}
