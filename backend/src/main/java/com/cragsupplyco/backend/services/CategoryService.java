059b8830e04f9a2b71ac510832926122a5a01fec
Britt Shook <brittanysshook@gmail.com>
2024-07-27 19:18:02 -0400

    add basic services for category

---
+++ backend/src/main/java/com/cragsupplyco/backend/services/CategoryService.java
@@ -0,0 +1,36 @@
+package com.cragsupplyco.backend.services;
+
+import java.util.Optional;
+
+import com.cragsupplyco.backend.models.Category;
+import com.cragsupplyco.backend.repositories.CategoryRepository;
+
+public class CategoryService {
+    private CategoryRepository repo;
+
+    public CategoryService(CategoryRepository repo) {
+        this.repo = repo;
+    }
+
+    public Iterable<Category> findAll() {
+        return repo.findAll();
+    }
+
+    public Optional<Category> findById(int id) {
+        return repo.findById(id);
+    }
+
+    public Category save(Category category) {
+        return repo.save(category);
+    }
+
+    public Category updateCategoryById(int id, Category category) {
+        category.setId(id);
+        return repo.save(category);
+    }
+
+    public void deleteById(int id) {
+        repo.deleteById(id);
+    }
+
+}
