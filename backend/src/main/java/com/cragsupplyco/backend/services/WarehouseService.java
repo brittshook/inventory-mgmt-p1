d142aafb532d5ba78b4bbd9d56e297dd5d4dd3d8
Britt Shook <brittanysshook@gmail.com>
2024-07-27 19:18:15 -0400

    add basic services for warehouse

---
+++ backend/src/main/java/com/cragsupplyco/backend/services/WarehouseService.java
@@ -0,0 +1,36 @@
+package com.cragsupplyco.backend.services;
+
+import java.util.Optional;
+
+import com.cragsupplyco.backend.models.Warehouse;
+import com.cragsupplyco.backend.repositories.WarehouseRepository;
+
+public class WarehouseService {
+    private WarehouseRepository repo;
+
+    public WarehouseService(WarehouseRepository repo) {
+        this.repo = repo;
+    }
+
+    public Iterable<Warehouse> findAll() {
+        return repo.findAll();
+    }
+
+    public Optional<Warehouse> findById(int id) {
+        return repo.findById(id);
+    }
+
+    public Warehouse save(Warehouse warehouse) {
+        return repo.save(warehouse);
+    }
+
+    public Warehouse updateWarehouseById(int id, Warehouse warehouse) {
+        warehouse.setId(id);
+        return repo.save(warehouse);
+    }
+
+    public void deleteById(int id) {
+        repo.deleteById(id);
+    }
+
+}
