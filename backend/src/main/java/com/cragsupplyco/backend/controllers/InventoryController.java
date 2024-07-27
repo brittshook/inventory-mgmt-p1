a281394c96837d0d8800bb59b1f454810560e4d4
Britt Shook <brittanysshook@gmail.com>
2024-07-27 19:58:00 -0400

    add missing rest controller annotations and request mapping uri

--- backend/src/main/java/com/cragsupplyco/backend/controllers/InventoryController.java
+++ backend/src/main/java/com/cragsupplyco/backend/controllers/InventoryController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cragsupplyco.backend.dtos.UpdateQuantityRequestDto;
 import com.cragsupplyco.backend.models.Inventory;
@@ -19,6 +21,8 @@ import com.cragsupplyco.backend.services.InventoryService;
 
 import jakarta.validation.Valid;
 
+@RestController
+@RequestMapping("/inventory")
 public class InventoryController {
     private InventoryService service;
 
--- backend/src/main/java/com/cragsupplyco/backend/controllers/ProductController.java
+++ backend/src/main/java/com/cragsupplyco/backend/controllers/ProductController.java
@@ -10,13 +10,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cragsupplyco.backend.models.Product;
 import com.cragsupplyco.backend.services.ProductService;
 
 import jakarta.validation.Valid;
 
+@RestController
+@RequestMapping("/product")
 public class ProductController {
     private ProductService service;
 
