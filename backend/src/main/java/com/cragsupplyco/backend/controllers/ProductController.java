0b49734ca4b495a60051d966f331f0b988a1a5ae
Britt Shook <brittanysshook@gmail.com>
2024-07-27 19:52:40 -0400

    add basic get, post methods for / eendpoint, get, put, delete for /id

--- backend/src/main/java/com/cragsupplyco/backend/controllers/ProductController.java
+++ backend/src/main/java/com/cragsupplyco/backend/controllers/ProductController.java
@@ -1,5 +1,59 @@
 package com.cragsupplyco.backend.controllers;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.cragsupplyco.backend.models.Product;
+import com.cragsupplyco.backend.services.ProductService;
+
+import jakarta.validation.Valid;
+
 public class ProductController {
-    
+    private ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public Iterable<Product> findAllCategories() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Product createProduct(@Valid @RequestBody Product product) {
+        return service.save(product);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> findProductById(@PathVariable int id) {
+        Optional<Product> product = service.findById(id);
+        if (product.isPresent())
+            return ResponseEntity.ok(product.get());
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void updateProductById(@PathVariable int id, @Valid @RequestBody Product Product) {
+        service.updateProductById(id, Product);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deleteProductById(@PathVariable int id) {
+        service.deleteById(id);
+    }
+
 }
