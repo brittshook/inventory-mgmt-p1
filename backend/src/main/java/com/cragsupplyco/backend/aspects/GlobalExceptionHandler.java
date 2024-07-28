540bbf211e15ed5f0290a1aa04b57ca1480cfc6c
Britt Shook <brittanysshook@gmail.com>
2024-07-28 00:25:10 -0400

    clean up error messages; global exception handling

--- backend/src/main/java/com/cragsupplyco/backend/aspects/GlobalExceptionHandler.java
+++ backend/src/main/java/com/cragsupplyco/backend/aspects/GlobalExceptionHandler.java
@@ -1,5 +1,16 @@
 package com.cragsupplyco.backend.aspects;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationError(Exception e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
 }
