7feee8e599159ded03f60add06af61f364ea16c2
Britt Shook <brittanysshook@gmail.com>
2024-07-30 03:13:56 -0400

    implement breadcrumb to products

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,3 +1,28 @@
-export const Products = () => {
-  return <h1>Products</h1>;
+import { Breadcrumb } from "../components/breadcrumb/Breadcrumb";
+import warehouseIcon from "../assets/icons/warehouse.svg";
+
+type props = {
+  warehouseName?: string;
+};
+
+export const Products = ({ warehouseName }: props) => {
+  return (
+    <Breadcrumb
+      items={[
+        {
+          href: "/warehouses",
+          title: (
+            <div className="breadcrumb-item">
+              <img src={warehouseIcon} alt="warehouse" />
+              <span>{warehouseName || "Warehouses"}</span>
+            </div>
+          ),
+        },
+        {
+          href: `/products?warehouse=${warehouseName || "all"}`,
+          title: "Products",
+        },
+      ]}
+    />
+  );
 };
