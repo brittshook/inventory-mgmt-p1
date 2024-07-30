f07153cb37ad55dbbd2c22ecd71d8b358af88ada
Britt Shook <brittanysshook@gmail.com>
2024-07-30 03:11:09 -0400

    move browser router to main to get context for link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import { Dashboard } from "./pages/Dashboard";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Login } from "./pages/Login";
 import { Warehouses } from "./pages/Warehouses";
 import { Inventory } from "./pages/Inventory";
 import { Products } from "./pages/Products";
-import { Sidebar } from "./components/Sidebar";
+import { Sidebar } from "./components/sidebar/Sidebar";
 
 function App() {
   // TODO: set to false once implementing users
@@ -15,15 +15,13 @@ function App() {
   return (
     <>
       {isLoggedIn && <Sidebar />}
-      <BrowserRouter>
-        <Routes>
-          {isLoggedIn && <Route path="/" element={<Dashboard />} />}
-          {!isLoggedIn && <Route path="/" element={<Login />} />}
-          <Route path="/warehouses" element={<Warehouses />} />
-          <Route path="/inventory" element={<Inventory />} />
-          <Route path="/products" element={<Products />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        {isLoggedIn && <Route path="/" element={<Dashboard />} />}
+        {!isLoggedIn && <Route path="/" element={<Login />} />}
+        <Route path="/warehouses" element={<Warehouses />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </>
   );
 }
