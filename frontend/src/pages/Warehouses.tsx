3f4b06b8f9c8f39ebd570804a4efc4e0fc7b87c1
Britt Shook <brittanysshook@gmail.com>
2024-07-31 17:28:35 -0400

    add warehouses page

--- frontend/src/pages/Warehouses.tsx
+++ frontend/src/pages/Warehouses.tsx
@@ -1,3 +1,48 @@
+import { Button } from "../components/Button";
+import { WarehouseDataType, getWarehouses } from "../api/warehouse";
+import { Card } from "../components/card/Card";
+import { useEffect, useState } from "react";
+
 export const Warehouses = () => {
-  return <h1>Warehouses</h1>;
+  const [warehouses, setWarehouses] = useState<WarehouseDataType[] | null>(
+    null
+  );
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getWarehouses();
+        setWarehouses(result);
+      } catch (e) {
+        e instanceof Error && setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  // TODO: make error message an alert
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <section id="warehouses">
+      <div className="section-heading">
+        <h1>Warehouses</h1>
+        <Button type="primary">Add Warehouse</Button>
+      </div>
+      <section className="cards">
+        {warehouses?.map((warehouse) => (
+          <Card
+            key={warehouse.id}
+            loaded={loading}
+            path={`/inventory?warehouse=${warehouse.id}`}
+            title={warehouse.name}
+          ></Card>
+        ))}
+      </section>
+    </section>
+  );
 };
