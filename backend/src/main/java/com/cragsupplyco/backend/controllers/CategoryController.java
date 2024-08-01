7e264b1b9d886a0c2ad20bd9732961d8fd03f6b2
Britt Shook <brittanysshook@gmail.com>
2024-07-31 21:13:32 -0400

    add json view annotation to control serialization

--- backend/src/main/java/com/cragsupplyco/backend/controllers/CategoryController.java
+++ backend/src/main/java/com/cragsupplyco/backend/controllers/CategoryController.java
@@ -45,6 +45,7 @@ public class CategoryController {
     }
 
     @GetMapping("/{id}")
+    @JsonView(Views.Internal.class)
     public ResponseEntity<Category> findCategoryById(@PathVariable int id) {
         Optional<Category> category = service.findById(id);
         if (category.isPresent())
