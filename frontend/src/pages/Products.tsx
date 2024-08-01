0c6a10a39d1be824dd6a6716f573d85efd22b358
Britt Shook <brittanysshook@gmail.com>
2024-08-01 16:33:59 -0400

    show cards for product categories

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,28 +1,75 @@
-import { Breadcrumb } from "../components/breadcrumb/Breadcrumb";
-import warehouseIcon from "../assets/icons/warehouse.svg";
+import { ButtonWithModal } from "../components/ButtonWithModal";
+import { CategoryDataType, getCategories } from "../api/category";
+import { Card } from "../components/card/Card";
+import { useEffect, useState } from "react";
+import gif404 from "/oops.gif";
+import { Form, Input } from "antd";
 
-type props = {
-  warehouseName?: string;
-};
+export const Products = () => {
+  const [categories, setCategories] = useState<CategoryDataType[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
 
-export const Products = ({ warehouseName }: props) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getCategories();
+        setCategories(result);
+      } catch (e) {
+        e instanceof Error && setError(e);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  // TODO: make error message an alert
+  if (error)
+    return (
+      <>
+        {console.log(error.message)}
+        <img
+          src={gif404}
+          alt="Animated image captioned 'file that under oops'"
+        />
+      </>
+    );
+
   return (
-    <Breadcrumb
-      items={[
-        {
-          href: "/warehouses",
-          title: (
-            <div className="breadcrumb-item">
-              <img src={warehouseIcon} alt="warehouse" />
-              <span>{warehouseName || "Warehouses"}</span>
-            </div>
-          ),
-        },
-        {
-          href: `/products?warehouse=${warehouseName || "all"}`,
-          title: "Products",
-        },
-      ]}
-    />
+      <section id="products">
+        <div className="section-heading">
+          <h1>Products</h1>
+          <ButtonWithModal
+            buttonText="Add Category"
+            buttonType="primary"
+            title="New Product Category"
+            onCreate={() => {
+              console.log("submit");
+            }}
+            modalButtonText="Create"
+          >
+            <Form.Item
+              label="Category Name"
+              name="name"
+              rules={[
+                { required: true, message: "Please input the category name" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </ButtonWithModal>
+        </div>
+        <section className="cards">
+          {categories?.map((category) => (
+            <Card
+              key={category.id}
+              loaded={loading}
+              path={`/inventory?category=${category.id}`}
+              title={category.name}
+            ></Card>
+          ))}
+        </section>
+      </section>
   );
 };
