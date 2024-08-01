5ed606a4670059cc2df7ddb2b25588bb3021968b
Britt Shook <brittanysshook@gmail.com>
2024-07-31 21:12:54 -0400

    fix handler method name typo

--- backend/src/main/java/com/cragsupplyco/backend/controllers/WarehouseController.java
+++ backend/src/main/java/com/cragsupplyco/backend/controllers/WarehouseController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cragsupplyco.backend.models.Product;
 import com.cragsupplyco.backend.models.Views;
 import com.cragsupplyco.backend.models.Warehouse;
 import com.cragsupplyco.backend.services.WarehouseService;
@@ -34,7 +36,7 @@ public class WarehouseController {
 
     @GetMapping
     @JsonView(Views.Public.class)
-    public Iterable<Warehouse> findAllCategories() {
+    public Iterable<Warehouse> findAllWarehouses() {
         return service.findAll();
     }
 
