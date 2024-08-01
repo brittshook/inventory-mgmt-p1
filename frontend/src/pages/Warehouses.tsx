6d9e375cd09cd5ea478463926351c700874f5a2e
Britt Shook <brittanysshook@gmail.com>
2024-08-01 16:37:47 -0400

    add warehouse button with modal

--- frontend/src/pages/Warehouses.tsx
+++ frontend/src/pages/Warehouses.tsx
@@ -1,7 +1,18 @@
-import { Button } from "../components/Button";
+import { Form, Input, Select } from "antd";
 import { WarehouseDataType, getWarehouses } from "../api/warehouse";
+import { ButtonWithModal } from "../components/ButtonWithModal";
 import { Card } from "../components/card/Card";
 import { useEffect, useState } from "react";
+import { US_STATES_AND_DC } from "../utils/states";
+
+type WarehouseFormValues = {
+  name: string;
+  maxCapacity: number;
+  streetAddress: string;
+  city: string;
+  state: string;
+  zipCode: string;
+};
 
 export const Warehouses = () => {
   const [warehouses, setWarehouses] = useState<WarehouseDataType[] | null>(
@@ -31,7 +42,91 @@ export const Warehouses = () => {
     <section id="warehouses">
       <div className="section-heading">
         <h1>Warehouses</h1>
-        <Button type="primary">Add Warehouse</Button>
+        <ButtonWithModal
+          buttonText="Add Warehouse"
+          buttonType="primary"
+          title="New Warehouse"
+          onCreate={() => {
+            console.log("submit");
+          }}
+          modalButtonText="Create"
+        >
+          <>
+            <Form.Item
+              label="Warehouse Name"
+              name="name"
+              rules={[
+                { required: true, message: "Please input the warehouse name!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Max Capacity"
+              name="maxCapacity"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the maximum capacity!",
+                },
+              ]}
+            >
+              <Input type="number" />
+            </Form.Item>
+            <Form.Item
+              label="Street Address"
+              name="streetAddress"
+              rules={[
+                { required: true, message: "Please input the street address!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Input.Group compact>
+              <Form.Item
+                label="City"
+                name="city"
+                rules={[{ required: true, message: "Please input the city!" }]}
+              >
+                <Input style={{ width: 150 }} />
+              </Form.Item>
+              <Form.Item
+                label="State"
+                name="state"
+                rules={[
+                  { required: true, message: "Please select the state!" },
+                ]}
+              >
+                <Select
+                  style={{ width: 170, marginLeft: 15, marginRight: 15 }}
+                  placeholder="State"
+                  showSearch
+                  filterOption={(input, option) =>
+                    (`${option?.label}` ?? "")
+                      .toLowerCase()
+                      .includes(input.toLowerCase())
+                  }
+                  options={US_STATES_AND_DC.map((state) => {
+                    return {
+                      key: state.value,
+                      value: state.value,
+                      label: state.label,
+                    };
+                  })}
+                />
+              </Form.Item>
+              <Form.Item
+                label="Zip Code"
+                name="zipCode"
+                rules={[
+                  { required: true, message: "Please input the zip code!" },
+                ]}
+              >
+                <Input style={{ width: 120 }} />
+              </Form.Item>
+            </Input.Group>
+          </>
+        </ButtonWithModal>{" "}
       </div>
       <section className="cards">
         {warehouses?.map((warehouse) => (
