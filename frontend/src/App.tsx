c33ab5c0432c7701c02e88e0c2124a1474d48802
Britt Shook <brittanysshook@gmail.com>
2024-08-02 05:49:54 -0400

    remove unused variables for login features

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,12 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import { Dashboard } from "./pages/Dashboard";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { Login } from "./pages/Login";
 import { Warehouses } from "./pages/Warehouses";
 import { Inventory } from "./pages/Inventory";
 import { Products } from "./pages/Products";
 import { Sidebar } from "./components/sidebar/Sidebar";
 
 function App() {
-  // TODO: set to false once implementing users
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
   const [isCollapsed, setIsCollapsed] = useState(window.innerWidth < 900);
 
   const handleResize = () => {
@@ -29,20 +26,15 @@ function App() {
 
   return (
     <>
-      {isLoggedIn && (
-        <>
-          <Sidebar isCollapsed={isCollapsed} />
-          <main>
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/warehouses" element={<Warehouses />} />
-              <Route path="/inventory" element={<Inventory />} />
-              <Route path="/products" element={<Products />} />
-            </Routes>
-          </main>
-        </>
-      )}
-      {!isLoggedIn && <Route path="/" element={<Login />} />}
+      <Sidebar isCollapsed={isCollapsed} />
+      <main>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/warehouses" element={<Warehouses />} />
+          <Route path="/inventory" element={<Inventory />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </main>
     </>
   );
 }
