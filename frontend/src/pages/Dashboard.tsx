cac5f17339dae7fdc49dc0478c4ca12422aeaf1f
Britt Shook <brittanysshook@gmail.com>
2024-08-02 05:50:48 -0400

    add overview cards to dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,63 @@
+import { useEffect, useState } from "react";
+import { getInventory } from "../api/inventory";
+import { Card } from "antd";
+import { DataType } from "../components/DataTable";
+import { getWarehouses, WarehouseDataType } from "../api/warehouse";
+
 export const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  const [totalInventory, setTotalInventory] = useState<number>(0);
+  const [totalCapacity, setTotalCapacity] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const inventoryResult = await getInventory();
+      const total = inventoryResult.reduce(
+        (sum: number, item: DataType) => sum + item.quantity,
+        0
+      );
+      setTotalInventory(total);
+
+      const warehouseResult = await getWarehouses();
+      const capacity = warehouseResult.reduce(
+        (sum: number, warehouse: WarehouseDataType) =>
+          sum + warehouse.maxCapacity,
+        0
+      );
+      setTotalCapacity(capacity);
+    } catch (e) {
+      e instanceof Error && setError(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
+  return (
+    <section id="dashboard">
+      <h1>Dashboard</h1>
+      <Card
+        style={{ marginTop: 16 }}
+        type="inner"
+        title="Total Items in Inventory"
+        extra={<a href="#">More</a>}
+      >
+        <h1 style={{ padding: 20 }}>{totalInventory}</h1>
+      </Card>
+      <Card
+        style={{ marginTop: 16 }}
+        type="inner"
+        title="Total Max Capacity"
+        extra={<a href="#">More</a>}
+      >
+        <h1 style={{ padding: 20 }}>{totalCapacity}</h1>
+      </Card>
+    </section>
+  );
 };
