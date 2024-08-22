02831080d3b44635ffe8c9fdcabe6cc290af32cd
Britt Shook <brittanysshook@gmail.com>
2024-08-22 11:10:29 -0400

    add warehouse service tests

---
+++ backend/src/test/java/com/cragsupplyco/backend/services/WarehouseServiceTest.java
@@ -0,0 +1,125 @@
+package com.cragsupplyco.backend.services;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.cragsupplyco.backend.models.Warehouse;
+import com.cragsupplyco.backend.repositories.WarehouseRepository;
+
+public class WarehouseServiceTest {
+
+    @InjectMocks
+    private WarehouseService warehouseService;
+    private AutoCloseable closeable;
+
+    @Mock
+    private WarehouseRepository warehouseRepository;
+
+    @BeforeTest
+    public void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+    }
+
+    @AfterTest
+    public void teardown() throws Exception {
+        closeable.close();
+    }
+
+    @Test
+    public void testFindAll() {
+        Warehouse warehouse1 = new Warehouse();
+        Warehouse warehouse2 = new Warehouse();
+        Warehouse warehouse3 = new Warehouse();
+        List<Warehouse> exepctedCategories = Arrays.asList(warehouse1, warehouse2, warehouse3);
+
+        when(warehouseRepository.findAll()).thenReturn(exepctedCategories);
+        Iterable<Warehouse> result = warehouseService.findAll();
+
+        int count = 0;
+        for (Warehouse warehouse : result) {
+            count++;
+        }
+
+        assertTrue(count == exepctedCategories.size());
+    }
+
+    @Test
+    public void testFindByExistingName() {
+        String warehouseName = "CA1";
+        Warehouse expectedWarehouse = new Warehouse();
+        expectedWarehouse.setName(warehouseName);
+
+        when(warehouseRepository.findByName(warehouseName)).thenReturn(Optional.of(expectedWarehouse));
+        Optional<Warehouse> result = warehouseService.findByName(warehouseName);
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void testFindByNonExistentName() {
+        String warehouseName = "ST1";
+
+        Optional<Warehouse> result = warehouseService.findByName(warehouseName);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testFindWarehouseByExistingId() {
+        int warehouseId = 1;
+        Warehouse expectedWarehouse = new Warehouse();
+        expectedWarehouse.setId(warehouseId);
+
+        when(warehouseRepository.findById(warehouseId)).thenReturn(Optional.of(expectedWarehouse));
+        Optional<Warehouse> result = warehouseService.findById(warehouseId);
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    public void testFindWarehouseByNonExistentId() {
+        int warehouseId = 2;
+
+        Optional<Warehouse> result = warehouseService.findById(warehouseId);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testSaveWarehouse() {
+        int warehouseId = 4;
+        Warehouse expectedWarehouse = new Warehouse();
+        expectedWarehouse.setId(warehouseId);
+
+        when(warehouseRepository.save(expectedWarehouse)).thenReturn(expectedWarehouse);
+        Warehouse result = warehouseService.save(expectedWarehouse);
+        assertTrue(result.equals(expectedWarehouse));
+    }
+
+    @Test
+    public void testUpdateWarehouseById() {
+        int warehouseId = 1;
+        Warehouse expectedWarehouse = new Warehouse();
+        expectedWarehouse.setId(warehouseId);
+
+        when(warehouseRepository.save(expectedWarehouse)).thenReturn(expectedWarehouse);
+        Warehouse result = warehouseService.updateWarehouseById(warehouseId, expectedWarehouse);
+        assertTrue(result.equals(expectedWarehouse));
+    }
+
+    @Test
+    public void testDeleteWarehouseById() {
+        int warehouseId = 1;
+        warehouseService.deleteById(warehouseId);
+        verify(warehouseRepository).deleteById(warehouseId);
+    }
+
+}
