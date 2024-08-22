fbf8c345663fb7b2fe0f6f8e2e1e6b1792b73c1d
Britt Shook <brittanysshook@gmail.com>
2024-08-22 11:05:27 -0400

    add teardown, make test categories local vars

--- backend/src/test/java/com/cragsupplyco/backend/services/CategoryServiceTest.java
+++ backend/src/test/java/com/cragsupplyco/backend/services/CategoryServiceTest.java
@@ -11,7 +11,8 @@ import java.util.Optional;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.cragsupplyco.backend.models.Category;
@@ -21,36 +22,29 @@ public class CategoryServiceTest {
 
     @InjectMocks
     private CategoryService categoryService;
+    private AutoCloseable closeable;
 
     @Mock
     private CategoryRepository categoryRepository;
 
-    private Category category1;
-    private Category category2;
-    private Category category3;
-
-    @BeforeMethod
+    @BeforeTest
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
+        closeable = MockitoAnnotations.openMocks(this);
+    }
 
-        this.category1 = new Category();
-        this.category1.setId(1);
-        this.category1.setName("Crampons");
-
-        this.category2 = new Category();
-        this.category2.setId(2);
-        this.category2.setName("Helmets");
-
-        this.category3 = new Category();
-        this.category3.setId(3);
-        this.category3.setName("Climbing Shoes");
+    @AfterTest
+    public void teardown() throws Exception {
+        closeable.close();
     }
 
     @Test
     public void testFindAll() {
-        List<Category> categories = Arrays.asList(category1, category2, category3);
-        when(categoryRepository.findAll()).thenReturn(categories);
+        Category category1 = new Category();
+        Category category2 = new Category();
+        Category category3 = new Category();
+        List<Category> exepctedCategories = Arrays.asList(category1, category2, category3);
 
+        when(categoryRepository.findAll()).thenReturn(exepctedCategories);
         Iterable<Category> result = categoryService.findAll();
 
         int count = 0;
@@ -58,57 +52,74 @@ public class CategoryServiceTest {
             count++;
         }
 
-        assertTrue(count == categories.size());
+        assertTrue(count == exepctedCategories.size());
     }
 
     @Test
     public void testFindByExistingName() {
-        when(categoryRepository.findByName(category1.getName())).thenReturn(Optional.of(category1));
-        Optional<Category> result = categoryService.findByName(category1.getName());
+        String categoryName = "Climbing Shoes";
+        Category expectedCategory = new Category();
+        expectedCategory.setName(categoryName);
+
+        when(categoryRepository.findByName(categoryName)).thenReturn(Optional.of(expectedCategory));
+        Optional<Category> result = categoryService.findByName(categoryName);
         assertTrue(result.isPresent());
     }
 
     @Test
     public void testFindByNonExistentName() {
-        Optional<Category> result = categoryService.findByName("Name");
+        String categoryName = "Helmets";
+
+        Optional<Category> result = categoryService.findByName(categoryName);
         assertTrue(result.isEmpty());
     }
 
     @Test
     public void testFindCategoryByExistingId() {
-        when(categoryRepository.findById(category1.getId())).thenReturn(Optional.of(category1));
-        Optional<Category> result = categoryService.findById(category1.getId());
+        int categoryId = 1;
+        Category expectedCategory = new Category();
+        expectedCategory.setId(categoryId);
+
+        when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(expectedCategory));
+        Optional<Category> result = categoryService.findById(categoryId);
         assertTrue(result.isPresent());
     }
 
     @Test
     public void testFindCategoryByNonExistentId() {
-        Optional<Category> result = categoryService.findById(1);
+        int categoryId = 2;
+
+        Optional<Category> result = categoryService.findById(categoryId);
         assertTrue(result.isEmpty());
     }
 
     @Test
     public void testSaveCategory() {
-        Category newCategory = new Category();
-        newCategory.setId(4);
-        newCategory.setName("New Category");
-        when(categoryRepository.save(newCategory)).thenReturn(newCategory);
-        Category result = categoryService.save(newCategory);
-        assertTrue(result.equals(newCategory));
+        int categoryId = 4;
+        Category expectedCategory = new Category();
+        expectedCategory.setId(categoryId);
+
+        when(categoryRepository.save(expectedCategory)).thenReturn(expectedCategory);
+        Category result = categoryService.save(expectedCategory);
+        assertTrue(result.equals(expectedCategory));
     }
 
     @Test
     public void testUpdateCategoryById() {
-        category3.setName("Updated Category");
-        when(categoryRepository.save(category3)).thenReturn(category3);
-        Category result = categoryService.updateCategoryById(category3.getId(), category3);
-        assertTrue(result.equals(category3));
+        int categoryId = 1;
+        Category expectedCategory = new Category();
+        expectedCategory.setId(categoryId);
+
+        when(categoryRepository.save(expectedCategory)).thenReturn(expectedCategory);
+        Category result = categoryService.updateCategoryById(categoryId, expectedCategory);
+        assertTrue(result.equals(expectedCategory));
     }
 
     @Test
     public void testDeleteCategoryById() {
-        categoryService.deleteById(category3.getId());
-        verify(categoryRepository).deleteById(category3.getId());
+        int categoryId = 1;
+        categoryService.deleteById(categoryId);
+        verify(categoryRepository).deleteById(categoryId);
     }
 
 }
