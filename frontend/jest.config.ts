21e702d3ac6470f862f6d621adee526ba44e6b56
Britt Shook <brittanysshook@gmail.com>
2024-08-24 15:49:04 -0400

    configure reports to get coverage

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,12 +1,14 @@
-export default {
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
       {
-        tsconfig: 'tsconfig.app.json',
+        tsconfig: "tsconfig.app.json",
         diagnostics: false,
         astTransformers: {
           before: [
@@ -20,8 +22,8 @@ export default {
                     VITE_AUTH0_CLIENT_ID: "placeholder",
                     VITE_AUTH0_REDIRECT_URI: "placeholder",
                     VITE_AUTH0_AUDIENCE: "placeholder",
-                  }
-                }
+                  },
+                },
               },
             },
           ],
@@ -30,7 +32,13 @@ export default {
     ],
   },
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|webp|svg|css)$": "<rootDir>/src/test/__mocks__/fileMock.js",
-    '^@/(.*)': '<rootDir>/src/$1'
+    "\\.(jpg|jpeg|png|gif|webp|svg|css)$":
+      "<rootDir>/src/test/__mocks__/fileMock.js",
+    "^@/(.*)": "<rootDir>/src/$1",
   },
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["lcov", "json", "text"],
 };
+
+export default config;
