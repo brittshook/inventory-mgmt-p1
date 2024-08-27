8d4f3c585602f08d31f66aa3675e1235543df3ce
Britt Shook <brittanysshook@gmail.com>
2024-08-26 22:27:03 -0400

    update with test id prop

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -3,10 +3,14 @@ import { Button as ButtonElement } from "antd";
 type props = {
   type?: "primary";
   children: string;
-  icon?: string;
   disabled?: boolean;
+  testId?: string;
 };
 
-export const Button = ({ children, type }: props) => {
-  return <ButtonElement type={type}>{children}</ButtonElement>;
+export const Button = ({ children, type, disabled, testId }: props) => {
+  return (
+    <ButtonElement data-testid={testId} type={type} disabled={disabled}>
+      {children}
+    </ButtonElement>
+  );
 };
