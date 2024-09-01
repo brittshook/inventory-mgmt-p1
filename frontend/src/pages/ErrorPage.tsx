9408fe9d2c937ec42ff81a592643fdc536860851
Britt Shook <brittanysshook@gmail.com>
2024-09-01 02:40:55 -0400

    refactor with overlay

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,24 +1,14 @@
-import { message } from "antd";
 import errorGIF from "/oops.gif";
-import { useEffect } from "react";
+import { ErrorOverlay } from "../components/ErrorOverlay";
 
 type props = {
   messageText: string;
 };
 
 export const ErrorPage = ({ messageText }: props) => {
-  const [messageApi, contextHolder] = message.useMessage();
-
-  useEffect(() => {
-    messageApi.open({
-      type: "error",
-      content: messageText,
-    });
-  }, [messageApi, messageText]);
-
   return (
     <>
-      {contextHolder}
+      <ErrorOverlay messageText={messageText} />
       <section>
         <h1>Sorry, looks like we encountered an error</h1>
         <img src={errorGIF} alt="" width={600} />
