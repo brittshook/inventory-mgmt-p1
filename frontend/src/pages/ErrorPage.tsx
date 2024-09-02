86ff944261a4156389e9871438fb2bee1ac4d70e
Britt Shook <brittanysshook@gmail.com>
2024-09-01 23:06:36 -0400

    move to bottom of page to avoid formatting issues

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -2,17 +2,20 @@ import errorGIF from "/oops.gif";
 import { ErrorOverlay } from "../components/ErrorOverlay";
 
 type props = {
-  messageText: string;
+  messageText?: string;
+  testId?: string;
 };
 
-export const ErrorPage = ({ messageText }: props) => {
+export const ErrorPage = ({ messageText, testId }: props) => {
   return (
-    <>
-      <ErrorOverlay messageText={messageText} />
-      <section>
-        <h1>Sorry, looks like we encountered an error</h1>
-        <img src={errorGIF} alt="" width={600} />
-      </section>
-    </>
+    <section data-testid={testId}>
+      <h1>Sorry, looks like we encountered an error</h1>
+      <img src={errorGIF} alt="" width={600} />
+      {messageText && (
+        <div data-testid="error-overlay">
+          <ErrorOverlay messageText={messageText} />
+        </div>
+      )}
+    </section>
   );
 };
