d52535f01f845aa38137c83e136334229f012934
Daniel Maramba <daniel413x@gmail.com>
2024-09-04 00:15:25 -0400

    add additional id sequence resetting

--- backend/src/main/java/com/cragsupplyco/backend/repositories/InventoryRepository.java
+++ backend/src/main/java/com/cragsupplyco/backend/repositories/InventoryRepository.java
@@ -3,11 +3,14 @@ package com.cragsupplyco.backend.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cragsupplyco.backend.models.Inventory;
 import com.cragsupplyco.backend.models.Product;
 import com.cragsupplyco.backend.models.Warehouse;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
@@ -15,4 +18,10 @@ public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
 
     boolean existsByProductAndWarehouseAndSize(Product product, Warehouse warehouse, String size);
 
+    // reset the PSQL id incrementing sequence for the sake of test data and
+    // facilitating tests
+    @Modifying
+    @Transactional
+    @Query(value = "ALTER SEQUENCE inventory_id_seq RESTART WITH 1", nativeQuery = true)
+    void resetIdSequence();
 }
--- backend/src/main/java/com/cragsupplyco/backend/repositories/ProductRepository.java
+++ backend/src/main/java/com/cragsupplyco/backend/repositories/ProductRepository.java
@@ -3,9 +3,12 @@ package com.cragsupplyco.backend.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cragsupplyco.backend.models.Product;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
@@ -13,4 +16,10 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
         Optional<Product> findByBrandAndName(String brand, String name);
 
+        // reset the PSQL id incrementing sequence for the sake of test data and
+        // facilitating tests
+        @Modifying
+        @Transactional
+        @Query(value = "ALTER SEQUENCE product_id_seq RESTART WITH 1", nativeQuery = true)
+        void resetIdSequence();
 }
