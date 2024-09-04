51d5be0d49431f5ff211ee055d121f9a844adaeb
Britt Shook <brittanysshook@gmail.com>
2024-09-04 17:06:24 -0400

    add mapper methods

---
+++ backend/src/main/java/com/cragsupplyco/backend/mappers/ProductMapper.java
@@ -0,0 +1,34 @@
+package com.cragsupplyco.backend.mappers;
+
+import java.util.Optional;
+
+import com.cragsupplyco.backend.dtos.ProductRequestDto;
+import com.cragsupplyco.backend.models.Category;
+import com.cragsupplyco.backend.models.Product;
+import com.cragsupplyco.backend.services.CategoryService;
+
+import jakarta.persistence.EntityNotFoundException;
+
+public class ProductMapper {
+    private CategoryService categoryService;
+
+    public Product toProduct(ProductRequestDto productRequestDto) {
+        Product product = new Product();
+        product.setBrand(productRequestDto.getBrand());
+        product.setName(productRequestDto.getName());
+        product.setDescription(productRequestDto.getDescription());
+        product.setPrice(productRequestDto.getPrice());
+
+        Optional<Category> optionalCategory = categoryService.findById(productRequestDto.getCategory());
+
+        if (optionalCategory.isPresent()) {
+            Category category = optionalCategory.get();
+            product.setCategory(category);
+        } else {
+            throw new EntityNotFoundException("Category not found with ID: " + productRequestDto.getCategory());
+        }
+
+        return product;
+    }
+
+}
