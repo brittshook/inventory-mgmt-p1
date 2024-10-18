4e43de1d717ed203f82a9ad77fb75763ebd1874c
Britt Shook <brittanysshook@gmail.com>
2024-10-18 11:11:36 -0400

    remove div wrapped around skip nav to allow proper navigation uusing el

--- frontend/src/components/SkipNavigation.tsx
+++ frontend/src/components/SkipNavigation.tsx
@@ -8,16 +8,14 @@ export const SkipNavigation = ({ section }: props) => {
   const [isFocused, setIsFocused] = useState(false);
 
   return (
-    <div>
-      <a
-        id="skip-nav"
-        href={section}
-        className={!isFocused ? "visually-hidden" : ""}
-        onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
-      >
-        Skip to main content
-      </a>
-    </div>
+    <a
+      id="skip-nav"
+      href={section}
+      className={!isFocused ? "visually-hidden" : ""}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+    >
+      Skip to main content
+    </a>
   );
 };
