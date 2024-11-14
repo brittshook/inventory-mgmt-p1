c364af8ae71fe5da33f77b9dab8dc7544958c84e
Britt Shook <brittanysshook@gmail.com>
2024-11-14 12:37:16 -0500

    remove sublists, add aria labels, remove repetitive alt text

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import logoutIcon from "../../assets/icons/logout.svg";
 import inventoryIcon from "../../assets/icons/inventory.svg";
 import profileImage from "/sample_user.webp";
 import { Logo } from "../logo/Logo";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useScreenSize } from "../../context/ScreenSizeContext";
 
 type props = {
@@ -16,112 +16,65 @@ type props = {
 export const Sidebar = ({ testId }: props) => {
   const { isSmallerThan900 } = useScreenSize(); // Get screen size info from context
 
-  const path = useLocation().pathname;
-  const search = useLocation().search;
-
   return (
-    <aside
+    <nav
+      id="sidebar"
       data-testid={testId}
       className={isSmallerThan900 ? "collapsed" : "expanded"} // Render collapsed view when screen size < 900px
     >
-      <nav>
+      <div id="menu">
         <Link to="/">
           {/* Render short-form logo when screen size < 900px */}
           {isSmallerThan900 ? <Logo type="short" /> : <Logo type="full" />}
         </Link>
-        <hr />
+        <hr aria-hidden />
         {!isSmallerThan900 && <h2>General</h2>}
-        <ul>
-          <li>
-            <Link id={"dashboard"} to="/">
-              <img src={dashboardIcon} alt="dashboard" title="dashboard" />
+        <ul role="menubar">
+          <li role="none">
+            <Link id={"dashboard"} to="/" role="menuitem">
+              <img
+                src={dashboardIcon}
+                alt={isSmallerThan900 ? "dashboard" : ""}
+              />
               {/* Render only icons (no labels) in collapsed view */}
               {!isSmallerThan900 && "Dashboard"}
             </Link>
           </li>
-          <li>
-            <Link id={"products"} to="/products">
-              <img src={productIcon} alt="products" title="products" />
+          <li role="none">
+            <Link id={"products"} to="/products" role="menuitem">
+              <img src={productIcon} alt={isSmallerThan900 ? "products" : ""} />
               {!isSmallerThan900 && "Products"}
             </Link>
-            {/* Do not render sublists in collapsed view; only render sublist in expanded view on related pages */}
-            {!isSmallerThan900 &&
-              /(products|inventory\?category=)/.test(path + search) && (
-                <ul className="sublist">
-                  <li>
-                    <Link id={"inventory-products"} to="inventory?category=all">
-                      <svg
-                        width="20"
-                        height="33"
-                        viewBox="0 0 20 33"
-                        fill={
-                          /category=/.test(search)
-                            ? "var(--accent-color-blue)"
-                            : "none"
-                        }
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M1 0V18M1 18H10.0278M14.5 13C13.3065 13 12.1619 13.4741 11.318 14.318C10.4741 15.1619 10 16.3065 10 17.5C10 18.6935 10.4741 19.8381 11.318 20.682C12.1619 21.5259 13.3065 22 14.5 22C15.6935 22 16.8381 21.5259 17.682 20.682C18.5259 19.8381 19 18.6935 19 17.5C19 16.3065 18.5259 15.1619 17.682 14.318C16.8381 13.4741 15.6935 13 14.5 13Z"
-                          stroke="#60646A"
-                        />
-                      </svg>
-                      Inventory
-                    </Link>
-                  </li>
-                </ul>
-              )}
           </li>
-          <li>
-            <Link id={"warehouses"} to="/warehouses">
-              <img src={warehouseIcon} alt="warehouse" />
+          <li role="none">
+            <Link id={"warehouses"} to="/warehouses" role="menuitem">
+              <img
+                src={warehouseIcon}
+                alt={isSmallerThan900 ? "warehouses" : ""}
+              />
               {!isSmallerThan900 && "Warehouses"}
             </Link>
-            {/* Do not render sublists in collapsed view; only render sublist in expanded view on related pages */}
-            {!isSmallerThan900 &&
-              /(warehouses|inventory\?warehouse=)/.test(path + search) && (
-                <ul className="sublist">
-                  <li>
-                    <Link
-                      id={"inventory-warehouses"}
-                      to="inventory?warehouse=all"
-                    >
-                      <svg
-                        width="20"
-                        height="33"
-                        viewBox="0 0 20 33"
-                        fill={
-                          /warehouse=/.test(search)
-                            ? "var(--accent-color-blue)"
-                            : "none"
-                        }
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M1 0V18M1 18H10.0278M14.5 13C13.3065 13 12.1619 13.4741 11.318 14.318C10.4741 15.1619 10 16.3065 10 17.5C10 18.6935 10.4741 19.8381 11.318 20.682C12.1619 21.5259 13.3065 22 14.5 22C15.6935 22 16.8381 21.5259 17.682 20.682C18.5259 19.8381 19 18.6935 19 17.5C19 16.3065 18.5259 15.1619 17.682 14.318C16.8381 13.4741 15.6935 13 14.5 13Z"
-                          stroke="#60646A"
-                        />
-                      </svg>
-                      Inventory
-                    </Link>
-                  </li>
-                </ul>
-              )}
           </li>
-          {isSmallerThan900 && (
-            <li>
-              <Link id="all-inventory" to="inventory?category=all">
-                <img src={inventoryIcon} alt="inventory" title="inventory" />
-              </Link>
-            </li>
-          )}
+          <li role="none">
+            <Link
+              id="all-inventory"
+              to="inventory?category=all"
+              role="menuitem"
+            >
+              <img
+                src={inventoryIcon}
+                alt={isSmallerThan900 ? "inventory" : ""}
+              />
+              {!isSmallerThan900 && "Inventory"}
+            </Link>
+          </li>
         </ul>
-      </nav>
+      </div>
       <div id="user-account">
         {/* Do not render user details in collapsed view */}
         {!isSmallerThan900 && (
           <div id="user-details">
-            <img id="profile" src={profileImage} alt="profile" />
+            <img id="profile" src={profileImage} alt="profile picture" />
             <div className="text">
               <h3>Jo Klein</h3>
               <p>Administrator</p>
@@ -130,6 +83,6 @@ export const Sidebar = ({ testId }: props) => {
         )}
         <img src={logoutIcon} alt="Logout" />
       </div>
-    </aside>
+    </nav>
   );
 };
